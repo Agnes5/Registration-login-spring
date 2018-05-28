dc7205224e99e0ebcc3bdf37e4f4847354ad5d69
Agnieszka <agawarchol15@gmail.com>
2018-05-28 14:20:45 +0200

    init commit

---
+++ src/main/java/model/User.java
@@ -0,0 +1,15 @@
+package model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.Set;
+
+@Entity
+@Table(name = "user")
+public class User {
+    private Long id;
+    private String username;
+    private String password;
+    private String passwordConfirm;
+    private Set<Role> roles;
+}
